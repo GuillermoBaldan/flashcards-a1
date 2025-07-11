3833053b829ac96d123e3425e49bda577815d0ab
Guillermo <guillermobaldan@gmail.com>
2025-07-11 17:22:48 +0200

    modifications on menu for editing

--- flashcards-a1-frontend/src/components/CardItem.tsx
+++ flashcards-a1-frontend/src/components/CardItem.tsx
@@ -130,9 +130,9 @@ const CardItem: React.FC<CardItemProps> = ({ card, onCardDeleted }) => {
         <div
           ref={menuRef}
           className="absolute top-10 bg-white border border-gray-200 rounded-md shadow-lg z-30"
-          style={{ right: '3rem' }}
+          style={{ right: '3rem', width: 'auto', margin: '0' }}
         >
-          <ul className="py-1">
+          <ul className="py-1" style={{ margin: '0', padding: '0'}}>
             <li>
               <button
                 className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
